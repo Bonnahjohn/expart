f092ab10237cede680790eb6da739ffaca195917
bonnah john <bonnahpoku@gmail.com>
2022-11-28 21:56:40 -0800

    space-around change to space evenly

--- app.js
+++ app.js
@@ -4,3 +4,9 @@ burger.addEventListener("click", () => {
   navLinks.classList.toggle("navLinks");
   burger.classList.toggle("take");
 });
+
+// navBar bg-color change
+// window.addEventListener("scroll", () => {
+//   let nav = document.querySelector(".nav");
+//   nav.classList.toggle("navColor", scrollY > 0);
+// });
